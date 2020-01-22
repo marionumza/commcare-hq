3d6fa8c91d394339d23c05e54665fd73e4b5eae6
Jennifer Schweers <orange.jenny@gmail.com>
2020-01-21 21:17:24 -0500

    Added save to SQL

--- corehq/apps/registration/models.py
+++ corehq/apps/registration/models.py
@@ -94,3 +94,22 @@ class RegistrationRequest(Document):
             reduce=False,
             include_docs=True).first()
         return result
+
+    def save(self, *args, **kwargs):
+        # Save to SQL
+        model, created = SQLRegistrationRequest.objects.update_or_create(
+            activation_guid=self.activation_guid,
+            defaults={
+                "tos_confirmed": self.tos_confirmed,
+                "request_time": self.request_time,
+                "request_ip": self.request_ip,
+                "confirm_time": self.confirm_time,
+                "confirm_ip": self.confirm_ip,
+                "domain": self.domain,
+                "new_user_username": self.new_user_username,
+                "requesting_user_username": self.requesting_user_username,
+            }
+        )
+
+        # Save to couch
+        super().save(*args, **kwargs)
